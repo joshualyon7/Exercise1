6613fd44b642b0d1f36e988725e7ca2bf5b48316
joshualyon7 <joshlyon@udel.edu>
2020-09-01 11:21:36 -0400

    Finished up q6

--- Dog.java
+++ Dog.java
@@ -1,15 +1,13 @@
 //Names: Michael DeSimone, Spencer Hanggodo, Josh Lyon
 import java.util.*;
 
+//q1
 class Dog{
 
     //Fields
     public String name;
 
-    //Constructors
-    public Dog(){
-
-    }
+    //Constructor
     public Dog(String Name){
         name = Name;
     }
@@ -20,30 +18,73 @@ class Dog{
     }
 
     public static void main(String[] args) {
-        Collection<Dog> dogs = new ArrayList<>();
-        dogs.add(new Dog("Fido"));
-        dogs.add(new Dog("Spot"));
-        dogs.add(new Dog("Lucy"));
-        System.out.println(dogs);
-        for (Dog d1 : dogs) {
+
+        //q2
+        Collection<Dog> dogCol = new ArrayList<>();
+        dogCol.add(new Dog("Fido"));
+        dogCol.add(new Dog("Spot"));
+        dogCol.add(new Dog("Lucy"));
+
+        //q3
+        System.out.println(dogCol);
+
+        //q4
+        for (Dog d1 : dogCol) {
             if(d1.toString() == "Spot"){
                 System.out.println(d1);
             }
         }
 
-        for(Iterator<Dog> dogIt = dogs.iterator(); dogIt.hasNext(); ){
-            /*if(dogIt.next().toString() == "Spot"){
-                dogs.add(new Dog());
+        //q5
+        
+        for(Iterator<Dog> dogIt = dogCol.iterator(); dogIt.hasNext(); ){
+            /*if(dogIt.next().toString() == "Spot"){ //concurrent modification exception
+                dogCol.add(new Dog("newDog"));
             }*/
-            /*if(dogIt.next.toString() == "Spot"){
-                dogIt.add(new Dog());
+            /*if(dogIt.next().toString() == "Spot"){ //iterators have no "add" method
+                dogIt.add(new Dog("newDog"));
             }*/
-            /*if (!(dogIt.next().toString() == "Spot")){
+            /*if (dogIt.next().toString() == "Spot"){ //concurrent modification exception
+                dogCol.remove(dogIt.next());
+            }*/
+            if (dogIt.next().toString() == "Spot"){ //this works
                 dogIt.remove();
-            }*/
-            
+                System.out.println(dogCol);
+            }
+            System.out.println("here");
         }
-    }
 
+        //q6
+        
+        HashSet<Dog> dogSet = new HashSet<>();
+        dogSet.add(new Dog("Fido"));
+        dogSet.add(new Dog("Spot"));
+        dogSet.add(new Dog("Lucy"));
+        
+        System.out.println(dogSet);
+
+        for (Dog d1 : dogSet) {
+            if(d1.toString() == "Spot"){
+                System.out.println(d1);
+            }
+        }
+
+        for(Iterator<Dog> dogIt = dogSet.iterator(); dogIt.hasNext(); ){
+            /*if(dogIt.next().toString() == "Spot"){ //concurrent modification exception
+                dogSet.add(new Dog("newDog"));
+            }*/
+            /*if(dogIt.next().toString() == "Spot"){ //iterators have no "add" method
+                dogIt.add(new Dog("newDog"));
+            }*/
+            /*if (dogIt.next().toString() == "Spot"){ //concurrent modification exception
+                dogSet.remove(dogIt.next());
+            }*/
+            if (dogIt.next().toString() == "Spot"){ //this works
+                dogIt.remove();
+                System.out.println(dogSet);
+            }
+        }
+
+    }
 
 }
